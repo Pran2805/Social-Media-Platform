f54c7acb2488a3a7bb33ded056a6f94b2e0b91e4
Pranav Shinde <96279193+Pran2805@users.noreply.github.com>
2024-08-01 09:56:49 +0000

    adding a gitignore

--- Server/src/controller/post.controller.js
+++ Server/src/controller/post.controller.js
@@ -1,6 +1,7 @@
 import { Post } from "../models/post.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
 import mongoose from "mongoose";
 
 const getPost = asyncHandler(async(req, res)=>{
